d2245572d5e65fcc97699ea1a6969fec58cd95d0
huiali <huiali@hotmail.com>
2021-05-06 23:09:52 +0800

    feat: Add ping controller

---
+++ Microservice-Container-Sample/Microservice-Container-Sample/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice_Container_Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Pong!";
+        }
+    }
+}
